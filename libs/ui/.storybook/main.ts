30f8e15a8da47cfe77f50d1abdcaeeeaf0970f4b
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-11-01 18:55:57 +0530

    add storybook

---
+++ libs/ui/.storybook/main.ts
@@ -0,0 +1,11 @@
+const config = {
+  framework: '@storybook/react-vite',
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+}
+
+export default config
